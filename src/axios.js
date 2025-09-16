bdc4cb53a5b8a5fc3be4f0c3e5d029603797381a
DevakiMathivarma <devakimathivarma@gmail.com>
2025-09-16 16:25:46 +0530

    Update axios.js
    updated url

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const TOKEN_KEY = "tpm_token"; // matches your code
 
 const instance = axios.create({
-  baseURL: "https://trainee-miniproject-tracker-backend.onrender.com/api/",
+  baseURL: "https://trainee-miniproject-tracker-backend.onrender.com",
 
   headers: {
     "Content-Type": "application/json",
